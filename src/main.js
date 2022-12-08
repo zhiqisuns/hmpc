95a46d46dd00d004f9965fdfa7b8af24dcbae0d2
zhiqisuns <1478509576@qq.com>
2022-12-08 16:20:25 +0800

    项目初始化

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// 将http挂载到原型上
+import http from './utils/request'
+Vue.prototype.$http = http
+
 Vue.config.productionTip = false
 
 new Vue({
---
+++ src/router/index.js
@@ -0,0 +1,10 @@
+import Vue from 'vue';
+import VueRouter from 'vue-Router'
+
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+    routers:[]
+})
+
+export default router
